91e3428a5703df32334ae94eeda897c713ea5527
Pratik-sys <pithore.pratik@gmail.com>
2021-11-02 10:44:16 +0530

    Bcrypt : hash password

--- controllers/User.js
+++ controllers/User.js
@@ -1,12 +1,14 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
 module.exports = {
   Register: async (req, res) => {
     try {
+      const hash = await bcrypt.hash(req.body.password, 10);
       const user = await new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: hash,
       }).save();
       return res.status(201).json({ msg: "User registered sucessfully !" });
     } catch (error) {
@@ -16,13 +18,15 @@ module.exports = {
   Login: async (req, res) => {
     try {
       const user = await User.findOne({ email: req.body.email });
+      comparehash = await bcrypt.compare(user.password, req.body.password);
       if (!user) {
         return res.status(404).json({ msg: "No such user available" });
-      } else if (user.password === req.body.password) {
+      } else if (comparehash) {
         return res.status(202).json({ msg: "User logged in sucessfully" });
       }
     } catch (error) {
-      return res.status().json({ msg: "Try login with proper credentials" });
+      console.log(error)
+      return res.status(404).json({ msg: "Try login with proper credentials" });
     }
   },
 };
